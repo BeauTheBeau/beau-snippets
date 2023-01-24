72206e679bb80dd7702ebf29f5d3cef91cb62bed
beau <beau7476@gmail.com>
2023-01-24 18:09:22 +0000

    Updated CSS for Layout Switcher and added a demo @ main.html

--- layout-switcher/main.js
+++ layout-switcher/main.js
@@ -1,19 +1,19 @@
 function layout(layout) {
-  const container = document.getElementById("container")
+	const container = document.getElementById("container")
 
-  switch (layout) {
-    case "grid":
-      container.classList.remove("list");
-      container.classList.add("grid");
-      break;
-    case "list":
-      container.classList.remove("grid");
-      container.classList.add("list");
-      break;
+	switch (layout) {
+		case "grid":
+			container.classList.remove("list");
+			container.classList.add("grid");
+			break;
+		case "list":
+			container.classList.remove("grid");
+			container.classList.add("list");
+			break;
 
-    default:
-      posts.classList.remove("grid");
-      posts.classList.remove("list");
-      break;
-  }
+		default:
+			container.classList.remove("grid");
+			container.classList.remove("list");
+			break;
+	}
 }
