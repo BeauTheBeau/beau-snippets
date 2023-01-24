7ed5daa3e1d176a99712b1981d8f9107c369e4fd
beau <beau7476@gmail.com>
2023-01-24 18:10:26 +0000

    Updated CSS for On Scroll > Conic Gradient 1  and added a demo @ main.html

--- on-scroll/conic-gradient-1/main.js
+++ on-scroll/conic-gradient-1/main.js
@@ -1,4 +1,17 @@
+function updateGradient(y) {
+	document.documentElement.style.setProperty('--rotation', y * 0.3 + 5 + 'deg');
+	
+	/* also change --gradient-perc-1, --gradient-perc-2, --gradient-perc-3
+	document.documentElement.style.setProperty('--gradient-perc-1', offset * 0.1 + 10 + '%');
+	document.documentElement.style.setProperty('--gradient-perc-2', offset * 0.2 + 20 + '%');
+	document.documentElement.style.setProperty('--gradient-perc-3', offset * 0.3 + 30 + '%');
+	
+	*/
+}
+
+
 window.addEventListener('scroll', () => {
-    let offset = window.pageYOffset;
-    document.documentElement.style.setProperty('--rotate', offset * 0.3 + 5 + 'deg');
+	updateGradient(window.scrollY);
 });
+
+updateGradient(window.scrollY);
